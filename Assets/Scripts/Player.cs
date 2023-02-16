799f9a637b95aab51be91b4955e6183d629a789d
Felipe Fantato <felipe.fantato3@gmail.com>
2023-02-15 22:05:22 -0300

    Criação das Paredes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float velocity = 4f;
+    float velocity = 10f;
     // Start is called before the first frame update
     void Start()
     {
