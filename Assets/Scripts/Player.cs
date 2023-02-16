faffb7873394ccca102ee98ba4f0a63e9c8c7cff
Felipe Fantato <felipe.fantato3@gmail.com>
2023-02-15 22:45:08 -0300

    Atualização na Camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float velocity = 10f;
+    [SerializeField] private float velocity = 10f;
     // Start is called before the first frame update
     void Start()
     {
